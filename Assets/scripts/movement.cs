89d47d7ed0c1633a62c15ec1e04698903f62f7e7
JoJoRogild <johanrogild@gmail.com>
2020-09-23 17:42:55 +0200

    movement

---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public int speed;
+
+    void Start()
+    {
+        Debug.Log("test");
+    }
+
+    void Update()
+    {
+
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 pos = transform.position;
+            pos.x = pos.x + -speed * Time.deltaTime;
+            transform.position = pos;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 pos = transform.position;
+            pos.x = pos.x + speed * Time.deltaTime;
+            transform.position = pos;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 pos = transform.position;
+            pos.y = pos.y + speed * Time.deltaTime;
+            transform.position = pos;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 pos = transform.position;
+            pos.y = pos.y + -speed * Time.deltaTime;
+            transform.position = pos;
+        }
+    }
+}
