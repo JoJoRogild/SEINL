e07a25030124a78138e77632b40dbe3799ced29c
Andersen <andersen@Andersens-MacBook-Pro.local>
2020-09-23 18:42:36 +0200

    hjælp!

---
+++ Assets/Scenes/dør åbenx.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class døråben : MonoBehaviour
+{
+    public GameObject player;
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x < transform.position.x + 30)
+        {
+            if (player.transform.position.x > transform.position.x - 30)
+
+                Debug.Log("åben");
+        }
+    }
+}
---
+++ Assets/Scenes/åben_x.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class åben_x : MonoBehaviour
+{
+    public bool åben_dør;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Debug.Log(collision.transform.tag);
+        if (collision.transform.tag == "player")
+        {
+            Debug.Log("åben plz");
+            åben_dør = true;
+            anim.SetBool("åben", true);
+        }
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("luk plz");
+        åben_dør = false;
+    }
+}
