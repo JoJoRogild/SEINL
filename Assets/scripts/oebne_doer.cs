0fd5a8cb2f74215fe9e0ffb92be511f84fc18e08
HoldenOA <rrrase.holden@gmail.com>
2020-10-28 18:59:17 +0100

    mur ha ha ha

---
+++ Assets/scripts/oebne_doer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oebne_doer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("if enter");
+        if(collision.transform.tag == "player")
+        {
+            anim.SetBool("åben", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("if exit");
+        if (other.transform.tag == "player")
+        {
+            anim.SetBool("åben", false);
+        }
+    }
+}
