5920d8fd4a0096310006ee70910b34297d86b8f3
HoldenOA <rrrase.holden@gmail.com>
2020-11-11 18:54:58 +0100

    h a

---
+++ Assets/scripts/vaksine_0.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vaksine_0 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("Vaksiner", 0);
+    }
+}
