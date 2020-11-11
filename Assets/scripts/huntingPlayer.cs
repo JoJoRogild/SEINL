f5d717743ef530b5939cca91dd7a46dfad78c503
JoJoRogild <johanrogild@gmail.com>
2020-11-11 18:55:13 +0100

    lort

--- Assets/scripts/huntingPlayer.cs
+++ Assets/scripts/huntingPlayer.cs
@@ -8,7 +8,7 @@ public class huntingPlayer : MonoBehaviour
     public GameObject enemy;
     public void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.tag == "player") { Player.GetComponent<player>().deadboy(); Debug.Log("fuck mesa dont know"); }
+        if (coll.tag == "player") { Player.GetComponent<player>().deadboy();}
         else if (coll.tag == "fuckDaGovernment" && GetComponent<BoxCollider2D>().IsTouching(coll)) { Destroy(GameObject.Find("fuckDaGovernment(Clone)")); enemy.GetComponent<fjendeAi>().setFalse(); enemy.GetComponent<fjendeAi>().setState(); }
     }
 }
